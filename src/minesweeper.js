fb36de1365d7ef4b7c39a69708f1c984cd7793c9
Josh Petras <joshpetras@gmail.com>
2017-10-20 12:48:35 +0200

    Day 7 project - step 11.

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,6 +1,12 @@
+const printBoard = board => {
+  console.log('Current Board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
 const board = [
   [' ', ' ', ' '],
   [' ', ' ', ' '],
   [' ', ' ', ' ']
 ];
-console.log(board);
+printBoard(board);
