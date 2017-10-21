3ff7483d6a63d94fe40b003c7d9bc501a38bf3bf
Josh Petras <joshpetras@gmail.com>
2017-10-21 18:09:50 +0200

    Day 10 and 11 project - step 11.

---
+++ src/minesweeper.js
@@ -0,0 +1,13 @@
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    const row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+};
+
+console.log(generatePlayerBoard(5, 4));
